834daff85037421841df2317e1d897f67bf9e546
arachi006 <tw1046972@swccd.edu>
2017-07-27 12:06:37 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Hai, Arachi inside";
+echo "Yuk mulai installnya";
+echo "Yuk mulai installnya";
+echo "Yuk mulai installnya";
+echo "Yuk mulai installnya";
+echo "Yuk mulai installnya";
+echo "Yuk mulai installnya";
+echo "Yuk mulai installnya";
+echo "Yuk mulai installnya";
+echo "Yuk mulai installnya";
+echo "Yuk mulai installnya";
+echo "Yuk mulai installnya";
+echo "Yuk mulai installnya";
+echo "Yuk mulai installnya";
+echo "Yuk mulai installnya";
+echo "Yuk mulai installnya";
+
+clear
+cd /bin
+wget -O menu https://github.com/arachi006/vps/raw/master/menu.sh
+chmod +x menu
+wget -O perbarui https://github.com/arachi006/vps/raw/master/daftar-menu.sh
+chmod +x perbarui
+perbarui
+cd
+menu
