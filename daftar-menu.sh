dbb84dd6323a4b2f6c6ecd97f09a1ba7b55f8749
arachi006 <tw1046972@swccd.edu>
2017-07-27 12:41:23 +0700

    Update daftar-menu.sh

--- daftar-menu.sh
+++ daftar-menu.sh
@@ -13,7 +13,7 @@ wget -O lock https://github.com/arachi006/vps/raw/master/lock.sh
 wget -O userlog https://github.com/arachi006/vps/raw/master/monuser.sh
 wget -O ubahpw https://github.com/arachi006/vps/raw/master/ubahpw.sh
 wget -O unlock https://github.com/arachi006/vps/raw/master/unlock.sh
-
+wget -O limit https://github.com/arachi006/vps/raw/master/limit.sh
 #beri permission
 chmod +x user
 chmod +x delexp
@@ -24,5 +24,5 @@ chmod +x lock
 chmod +x userlog
 chmod +x ubahpw
 chmod +x unlock
-
+chmod +x limit
 echo "Pembaruan menu sukses!!!"
